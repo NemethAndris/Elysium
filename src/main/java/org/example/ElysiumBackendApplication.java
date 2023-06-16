701992202c80518ecca88b5847ea7eb3a6bfab9c
Nemeth Andras <nthas13@gmail.com>
2023-06-16 14:38:16 +0200

    database config

--- src/main/java/org/example/ElysiumBackendApplication.java
+++ src/main/java/org/example/ElysiumBackendApplication.java
@@ -1,10 +1,11 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ElysiumBackendApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(ElysiumBackendApplication.class, args);
     }
 }
