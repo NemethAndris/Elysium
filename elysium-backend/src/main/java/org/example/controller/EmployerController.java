a8a08ae557840a70c9369d60f9bbf1992e02e291
Nemeth Andras <nthas13@gmail.com>
2023-06-22 13:01:46 +0200

    change url of contollers

--- elysium-backend/src/main/java/org/example/controller/EmployerController.java
+++ elysium-backend/src/main/java/org/example/controller/EmployerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/employer")
 public class EmployerController {
 
     private final EmployerService employerService;
@@ -15,5 +15,8 @@ public class EmployerController {
     public EmployerController( EmployerService employerService) {
         this.employerService = employerService;
     }
+
+
+
 
 }
